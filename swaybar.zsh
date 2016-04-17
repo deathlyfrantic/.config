b04bb817cdacd772ba5cb78ac0a50f4fb1c889f9
Zandr Martin <zandrmartin@gmail.com>
2016-04-17 17:30:28 -0500

    remove icons from swaybar, update irssi theme

--- swaybar.zsh
+++ swaybar.zsh
@@ -19,11 +19,11 @@ headphones () {
 
 volume () {
     local volume="$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
-    echo "$volume%"
+    echo "Volume $volume"
 }
 
 volume_icon () {
-    local volume=$1
+    local volume="$(volume)"
     local icon=""
     headphones
     if [[ $? == 1 ]]; then
@@ -79,14 +79,18 @@ segment () {
     local name=$1
     local text=$($1)
     local icon=$("$1_icon")
-    echo "{\"name\": \"$name\","
-    echo "\"full_text\": \"$icon $text\","
-    echo "\"short_text\": \"$text\","
-    echo "\"markup\": \"pango\"},"
+    if [[ $text != "" ]]; then
+        echo "{\"name\": \"$name\","
+        # echo "\"full_text\": \"<span font_family='FontAwesome'>$icon</span> $text\","
+        echo "\"full_text\": \"$text\","
+        echo "\"short_text\": \"$text\","
+        echo "\"separator_block_width\": 21,"
+        echo "\"markup\": \"pango\"},"
+    fi
 }
 
 echo '{ "version": 1 }'
-echo '[[]'
+echo '['
 while true; do
     echo ",["
     segment "song_info"
