c1cc9f37861b17806b0863e6aa9d86ff568d4a50
Zandr Martin <zandrmartin@gmail.com>
2016-09-09 16:19:51 -0500

    "upsteam" is not the same as "upstream"

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,12 +1,13 @@
+set __fish_git_prompt_show_informative_status 1
+
 set __fish_git_prompt_color_branch -o 'black'
 
 set __fish_git_prompt_char_dirtystate '+'
 set __fish_git_prompt_color_dirtystate -o 'blue'
 
-set __fish_git_prompt_char_upsteam_ahead 'A'
-set __fish_git_prompt_color_upsteam -o 'cyan'
-
-set __fish_git_prompt_char_upsteam_behind '<'
+set __fish_git_prompt_char_upstream_ahead '>'
+set __fish_git_prompt_char_upstream_behind '<'
+set __fish_git_prompt_color_upstream -o 'cyan'
 
 set __fish_git_prompt_char_cleanstate '='
 set __fish_git_prompt_color_cleanstate -o 'green'
@@ -19,17 +20,6 @@ set __fish_git_prompt_color_untrackedfiles -o 'magenta'
 
 set __fish_git_prompt_char_invalidstate '!'
 set __fish_git_prompt_color_invalidstate -o red
-
-set __fish_git_prompt_show_informative_status 'yes'
-
-# Status Chars
-# set __fish_git_prompt_char_stagedstate '→'
-# set __fish_git_prompt_char_untrackedfiles '☡'
-# set __fish_git_prompt_char_stashstate '↩'
-
-# ZSH_THEME_GIT_PROMPT_PREFIX="["
-# ZSH_THEME_GIT_PROMPT_SUFFIX="]"
-# ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg_bold[red]%}%{!%G%}"
 
 function fish_right_prompt
     set __fgpr (__fish_git_prompt)
