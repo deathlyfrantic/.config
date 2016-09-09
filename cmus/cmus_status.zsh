4774cce6c1b93af07dbf89b09fb622dce43d0ec5
Zandr Martin <git@zandr.me>
2016-09-09 16:41:38 -0500

    more miscellany

--- cmus/cmus_status.zsh
+++ cmus/cmus_status.zsh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 while test $# -ge 2
 do
 	eval _$1='$2'
@@ -11,11 +12,18 @@ then
     remove="--remove"
 elif [[ $_status == "playing" ]]
 then
-    status=""
+    __status=""
 else
-    status=" [$_status]"
+    __status=" [$_status]"
 fi
 
-output="$_artist - $_title$status"
+__title=
+
+output="$_artist - $_title$__status"
+output=$(echo $output | sed -e 's/&/\&amp\;/g')
+output=$(echo $output | sed -e 's/>/\&gt\;/g')
+output=$(echo $output | sed -e 's/</\&lt\;/g')
+output=$(echo $output | sed -e "s/'/\&apos\;/g")
+output=$(echo $output | sed -e 's/"/\&quot\;/g')
 
 ~/Code/swaystag/swaystag.py block --name "music" --sort_order 1 --full_text "$output" --short_text "$_title" $remove
