0e6a3680332beee2e94c88caf1286aed36edbf72
Zandr Martin <git@zandr.me>
2017-02-07 09:46:51 -0500

    all the changes

--- bin/weather_status.zsh
+++ bin/weather_status.zsh
@@ -1,8 +1,4 @@
 #!/usr/bin/env zsh
 _location=$(cat "$XDG_CONFIG_HOME/weather-location.txt")
-_weather=$(weather "$_location")
-
-if [[ $_weather != "" ]]
-then
-    ~/src/swaystag/swaystag.py block -n "weather" -f "Weather: $_weather" -st "$_weather" -o 997
-fi
+_weather=$(weather "$_location" 10)
+~/src/swaystag/swaystag.py block -n "weather" -f "$_weather" -st "$_weather" -o 997
