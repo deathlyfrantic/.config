83ed3699cedd27715603d09eade95578a3c3ce7a
Zandr Martin <zandrmartin@gmail.com>
2017-02-21 12:54:44 -0500

    miscellany

--- bin/weather_status.zsh
+++ bin/weather_status.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 _location=$(cat "$XDG_CONFIG_HOME/weather-location.txt")
 _weather=$(weather "$_location" 10)
-~/src/swaystag/swaystag.py block -n "weather" -f "$_weather" -st "$_weather" -o 997
+swaystag block -n "weather" -f "$_weather" -st "$_weather" -o 997
