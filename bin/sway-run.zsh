b6dbc5f8bf9b420a0019ce8a38d8a59b7a395f02
Zandr Martin <zandrmartin@gmail.com>
2017-05-17 21:38:00 -0400

    add x11 stuff

--- bin/sway-run.zsh
+++ bin/sway-run.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 _logfile="$XDG_CACHE_HOME"/sway-debug.log
-sway -d 2> $_logfile
+/usr/bin/sway -d 2> $_logfile
 
 if [[ $? != 0 ]]; then
     _time=$(date +'%Y%m%d%H%M%S')
--- bin/weather-status.zsh
+++ bin/weather-status.zsh
@@ -1,3 +1,4 @@
 _location=$(cat "$XDG_CONFIG_HOME/weather-location.txt")
 _weather=$(weather "$_location" 10)
-swaystag block -n "weather" -f "$_weather" -st "$_weather" -o 997
+_short=$(echo $_weather | cut -d' ' -f1)
+stag block -n "weather" -f "$_weather" -st "$_short" -o 997
