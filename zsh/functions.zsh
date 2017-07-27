d46a3c5dffe484a4d9c1864cc938b1aa9d492f53
Zandr Martin <zandrmartin@gmail.com>
2017-07-27 14:52:22 -0400

    zsh prompt changes mostly

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -32,24 +32,39 @@ function mkcd {
     mkdir -p $1 && cd $1
 }
 
+function set_up_prompt {
+    local _user="%{$fg[blue]%}%n"
+    local _at="%{$fg_bold[blue]%}@%{$reset_color%}"
+    local _dot=" %{$fg_bold[black]%}::%{$reset_color%} "
+    local _host="%{$fg[blue]%}%m"
+    local _path='%{$fg[white]%}${${(%):-%~}//\//%{$fg_bold[black]%\}/%{$reset_color%\}}'
+    local _green=$(echo -e '\e[38;2;138;226;52m')
+    local _base="$_path%(!.%{$fg_bold[red]%}.%{$_green%}) $%{$reset_color%} "
+
+    if [[ $SSH_CONNECTION != '' ]]; then
+        PROMPT=$_user$_at$_host$_dot$_base
+    elif [[ $USER != 'zandr' ]]; then
+        PROMPT=$_user$_dot$_base
+    else
+        PROMPT=$_base
+    fi
+
+    RPROMPT="%{$reset_color%}%D{%H}%{$fg_bold[black]%}:%{$reset_color%}%D{%M}%{$fg_bold[black]%}:%{$reset_color%}%D{%S}"
+
+    if [[ -x $(which gitprompt 2> /dev/null) ]]; then
+        RPROMPT='${$(gitprompt)} '$RPROMPT
+    fi
+
+    RPROMPT='${$(venv_prompt)}'$RPROMPT
+}
+
 function venv_prompt {
     if [[ -n "$VIRTUAL_ENV" ]];
     then
-        local vp=(
-            "%{$reset_color%}["
-            "%{$fg_bold[green]%}"
-            "$(basename $(dirname $VIRTUAL_ENV))"
-            "%{$reset_color%}|"
-            "%{$fg_bold[blue]%}"
-            "$(basename $VIRTUAL_ENV)"
-            "%{$reset_color%}] "
-            "%{$fg_bold[black]%}::"
-            "%{$reset_color%} "
-        )
-
-        for i in $vp;
-        do
-            echo -n $i
-        done
+        echo -n "%{$reset_color%}$(basename $(dirname $VIRTUAL_ENV))"
+        echo -n "%{$fg_bold[black]%}/"
+        echo -n "%{$fg_bold[blue]%}$(basename $VIRTUAL_ENV)"
+        echo -n "%{$fg_bold[black]%} ::"
+        echo -n "%{$reset_color%} "
     fi
 }
