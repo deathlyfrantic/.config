bb909c00cc6b9882cfb47add4d65aed96860d072
Zandr Martin <git@zandr.me>
2017-09-12 23:20:38 -0400

    initial macos commit

--- bin/sway-run.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-_logfile="$XDG_CACHE_HOME"/sway-debug.log
-/usr/bin/sway -d 2> $_logfile
-
-if [[ $? != 0 ]]; then
-    _time=$(date +'%Y%m%d%H%M%S')
-    cp $_logfile ~/sway-crash-$_time.log
-fi
