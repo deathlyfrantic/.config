824746991f9a4e60572c5958230d9ff16dee665d
Zandr Martin <git@zandr.me>
2018-09-09 10:01:45 -0400

    reformat functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -34,32 +34,32 @@ function set_up_prompt {
     local _at="%{$fg_bold[blue]%}@%{$reset_color%}"
     local _dot=" %{$fg_bold[black]%}::%{$reset_color%} "
     local _host="%{$fg[blue]%}%m"
-    local _path='%{$fg[white]%}${${(%):-%~}//\//%{$fg_bold[black]%\}/%{$reset_color%\}}'
-    local _jobs="%(1j.%{$fg_bold[black]%} :: %{$reset_color%}jobs%{$fg_bold[black]%}/%{$fg_bold[blue]%}%j.)"
+    local _path='%{$fg[white]%}${${(%):-%~}//\//"\
+"%{$fg_bold[black]%\}/%{$reset_color%\}}'
+    local _jobs="%(1j.%{$fg_bold[black]%} :: %{$reset_color%}"\
+"jobs%{$fg_bold[black]%}/%{$fg_bold[blue]%}%j.)"
 
-    if [[ $TERM == 'linux' || $TERM == 'xterm' ]];
-    then
+    if [[ $TERM == 'linux' || $TERM == 'xterm' ]]; then
         local _green=$fg_bold[green]
     else
         local _green=$(echo -e '\e[38;2;138;226;52m')
     fi
 
-    local _base="$_path$_jobs %(!.%{$fg_bold[red]%}#.%{$_green%}$)%{$reset_color%} "
+    local _base="$_path$_jobs %(!.%{$fg_bold[red]%}#.%{$_green%}$)"\
+"%{$reset_color%} "
 
-    if [[ $SSH_CONNECTION != '' ]];
-    then
+    if [[ $SSH_CONNECTION != '' ]]; then
         PROMPT=$_user$_at$_host$_dot$_base
-    elif [[ $USER != 'zandr' ]];
-    then
+    elif [[ $USER != 'zandr' ]]; then
         PROMPT=$_user$_dot$_base
     else
         PROMPT=$_base
     fi
 
-    RPROMPT="%{$reset_color%}%D{%H}%{$fg_bold[black]%}:%{$reset_color%}%D{%M}%{$fg_bold[black]%}:%{$reset_color%}%D{%S}"
+    RPROMPT="%{$reset_color%}%D{%H}%{$fg_bold[black]%}:"\
+"%{$reset_color%}%D{%M}%{$fg_bold[black]%}:%{$reset_color%}%D{%S}"
 
-    if [[ -x $(which gitprompt 2> /dev/null) ]];
-    then
+    if [[ -x $(which gitprompt 2> /dev/null) ]]; then
         RPROMPT='${$(gitprompt)}'$RPROMPT
     else
         RPROMPT='${$(git_prompt)}'$RPROMPT
@@ -69,8 +69,7 @@ function set_up_prompt {
 }
 
 function venv_prompt {
-    if [[ -n "$VIRTUAL_ENV" ]];
-    then
+    if [[ -n "$VIRTUAL_ENV" ]]; then
         prompt_color_echo $(basename $(dirname $VIRTUAL_ENV)) reset
         prompt_color_echo / black bold
         prompt_color_echo $(basename $VIRTUAL_ENV) blue bold
@@ -83,11 +82,9 @@ function prompt_color_echo {
     local color=$2
     local bold=$3
 
-    if [[ $color == "reset" ]];
-    then
+    if [[ $color == "reset" ]]; then
         local esc=$reset_color
-    elif [[ $bold != "" ]];
-    then
+    elif [[ $bold != "" ]]; then
         local esc=$fg_bold[$color]
     else
         local esc=$fg[$color]
@@ -98,14 +95,12 @@ function prompt_color_echo {
 
 function git_prompt {
     local branch=$(git symbolic-ref HEAD 2>&1)
-    if [[ $branch =~ "fatal: Not a git repository" ]];
-    then
+    if [[ $branch =~ "fatal: Not a git repository" ]]; then
         return
     fi
 
     # branch name (and ahead/behind if applicable)
-    if [[ $branch =~ "fatal: ref HEAD is not a symbolic ref" ]];
-    then
+    if [[ $branch =~ "fatal: ref HEAD is not a symbolic ref" ]]; then
         branch=":"$(git rev-parse --short HEAD)
         prompt_color_echo $branch reset
     else
@@ -116,13 +111,11 @@ function git_prompt {
         local behind=$(echo $ab | cut -f1 -d' ')
         local ahead=$(echo $ab | cut -f2 -d' ')
 
-        if [[ $behind > 0 ]];
-        then
+        if [[ $behind > 0 ]]; then
             prompt_color_echo "<$behind" red bold
         fi
 
-        if [[ $ahead > 0 ]];
-        then
+        if [[ $ahead > 0 ]]; then
             prompt_color_echo ">$ahead" cyan bold
         fi
     fi
@@ -134,39 +127,34 @@ function git_prompt {
 
     # staged
     local staged=$(git diff --staged --name-status | wc -l | tr -d '[:blank:]')
-    if [[ $staged > 0 ]];
-    then
+    if [[ $staged > 0 ]]; then
         prompt_color_echo "-$staged" yellow bold
         clean="no"
     fi
 
     # conflicts
     local conflicts=$(git diff --staged --name-status | grep -c '^U ')
-    if [[ $conflicts > 0 ]];
-    then
+    if [[ $conflicts > 0 ]]; then
         prompt_color_echo "!$conflicts" red bold
         clean="no"
     fi
 
     # changed
     local changed=$(git diff --name-status | wc -l | tr -d '[:blank:]')
-    if [[ $changed > 0 ]];
-    then
+    if [[ $changed > 0 ]]; then
         prompt_color_echo "+$changed" blue bold
         clean="no"
     fi
 
     # untracked
     local untracked=$(git status --porcelain | grep -c '^?? ')
-    if [[ $untracked > 0 ]];
-    then
+    if [[ $untracked > 0 ]]; then
         prompt_color_echo "_$untracked" magenta bold
         clean="no"
     fi
 
     # clean
-    if [[ $clean == "" ]];
-    then
+    if [[ $clean == "" ]]; then
         prompt_color_echo "=" green bold
     fi
 
@@ -178,15 +166,13 @@ function _git_prompt_ahead_behind {
     local branch=$1
     local remote=$(git config branch.$branch.remote)
 
-    if [[ $remote == "" ]];
-    then
+    if [[ $remote == "" ]]; then
         return
     fi
 
     local merge=$(git config branch.$branch.merge)
 
-    if [[ $remote == "." ]];
-    then
+    if [[ $remote == "." ]]; then
         local ref=$merge
     else
         local ref="refs/remotes/$remote/$(echo $merge | cut -d/ -f3)"
@@ -206,10 +192,8 @@ function zshaddhistory {
         '^b upgrade' '^brew upgrade'
         '^b cask upgrade' '^brew cask upgrade'
     )
-    for i in $boring;
-    do
-        if [[ $1 =~ $i ]];
-        then
+    for i in $boring; do
+        if [[ $1 =~ $i ]]; then
             return 1
         fi
     done
@@ -224,8 +208,7 @@ function timer {
     fi
     local pf=" %$(($COLUMNS / 2))s\r"
     printf "\n"
-    for i in $(seq $secs 1);
-    do
+    for i in $(seq $secs 1); do
         printf $pf $i
         sleep 1
     done
