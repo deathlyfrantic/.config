efebde9965e291b5d5e4c687bf8b51dd859a8722
Zandr Martin <git@zandr.me>
2019-01-31 07:13:09 -0500

    make signal titlebar dark

--- macos.sh
+++ macos.sh
@@ -11,3 +11,6 @@ defaults write com.apple.dashboard mcx-disabled -boolean true
 # time machine does not need to run every hour, that is ridiculous
 # does this work? idk
 sudo defaults write com.apple.backupd-auto Interval -int 43200
+
+# make signal title bar dark
+defaults write org.whispersystems.signal-desktop NSRequiresAquaSystemAppearance -bool false
