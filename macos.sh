a63edc43260badd11fa7ef44eec3b809868aad09
Zandr Martin <git@zandr.me>
2019-05-19 18:09:46 -0400

    miscellany!

--- macos.sh
+++ macos.sh
@@ -14,3 +14,36 @@ sudo defaults write com.apple.backupd-auto Interval -int 43200
 
 # make signal title bar dark
 defaults write org.whispersystems.signal-desktop NSRequiresAquaSystemAppearance -bool false
+
+# disable google analytics on porting kit
+defaults write ~/Library/Preferences/edu.ufrj.vitormm.Porting-Kit.plist "Disable Google Analytics API" -bool true
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# base brew packages i need
+brew install git
+brew install mutt urlview w3m
+brew install neovim
+brew install ripgrep
+brew install tmux
+brew install zsh zsh-completions
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install tree
+brew cask install alacritty
+brew cask install firefox
+brew cask install iterm2
+brew cask install signal
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh
+
+# make zsh use .config dir
+sudo echo 'export ZDOTDIR=$HOME/.config/zsh' >> /etc/zshenv
+
+# link custom firefox css
+ln -s ~/.config/firefox-userChrome.css ~/Library/Application Support/Firefox/Profile/$profile/chrome/userChrome.css
+
+# set up readline-style key bindings
+[ -d ~/Library/KeyBindings ] || mkdir ~/Library/KeyBindings
+cp ~/.config/DefaultKeyBinding.dict ~/Library/KeyBindings/
