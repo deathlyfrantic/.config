aded11b812c74e242c810108ae0d78def896f4be
Zandr Martin <zandrmartin@gmail.com>
2019-06-17 08:04:02 -0400

    simplify python startup.py

--- python/startup.py
+++ python/startup.py
@@ -1,9 +1,10 @@
 import atexit
 import os
 import readline
+import rlcompleter
 import sys
 
-histfile = os.path.join(os.getenv("XDG_DATA_HOME"), "python", "history")
+histfile = os.path.join(os.getenv("XDG_DATA_HOME"), "python-history")
 
 try:
     readline.read_history_file(histfile)
@@ -14,19 +15,8 @@ except:
 
 atexit.register(readline.write_history_file, histfile)
 
-
-def register_readline_completion():
-    # rlcompleter must be loaded for Python-specific completion
-    try:
-        import readline, rlcompleter
-    except ImportError:
-        return
-    # Enable tab-completion
-    readline_doc = getattr(readline, "__doc__", "")
-    if readline_doc is not None and "libedit" in readline_doc:
-        readline.parse_and_bind("bind ^I rl_complete")
-    else:
-        readline.parse_and_bind("tab: complete")
-
-
-sys.__interactivehook__ = register_readline_completion
+# Enable tab-completion
+if "libedit" in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
