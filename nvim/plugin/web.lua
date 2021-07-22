49812b202743423629581c64188c24d2fdf5abdb
Zandr Martin <zandrmartin@gmail.com>
2021-07-21 20:44:17 -0400

    Translate web.vim to lua

---
+++ nvim/plugin/web.lua
@@ -0,0 +1,68 @@
+local search_url = "https://duckduckgo.com/?q=%s"
+
+local function browser(url)
+  local open = "xdg-open"
+  if vim.fn.has("mac") == 1 then
+    open = "open -g"
+  end
+  os.execute(open .. " " .. vim.fn.shellescape(url, true))
+end
+
+local function search(url)
+  if not vim.startswith(url, "http") then
+    url = search_url:format(url:gsub(" ", "+"))
+  end
+  browser(url)
+end
+
+local function operator(kind)
+  local error = function(msg)
+    vim.api.nvim_err_writeln(
+      msg or "multiline selections do not work with this operator"
+    )
+  end
+  if kind:match("[V]") then
+    error()
+    return
+  end
+  local regsave = vim.fn.getreg("@")
+  local selsave = vim.o.selection
+  vim.o.selection = "inclusive"
+  if kind == "v" then
+    vim.cmd([[silent execute "normal! y"]])
+  else
+    vim.cmd([[silent execute "normal! `[v`]y"]])
+  end
+  local url = vim.fn.getreg("@"):trim()
+  if url:match("\n") then
+    error()
+    return
+  end
+  vim.o.selection = selsave
+  vim.fn.setreg("@", regsave)
+  if url == "" then
+    error("no selection")
+    return
+  end
+  search(url)
+end
+
+_G.web = { operator = operator, browser = browser, search = search }
+
+-- Browse alias is for Fugitive's Gbrowse
+vim.cmd("command! -nargs=1 Browser Web <args>")
+vim.cmd("command! -nargs=1 Web call v:lua.web.browser(<f-args>)")
+vim.cmd("command! -nargs=1 Search call v:lua.web.search(<f-args>)")
+
+vim.api.nvim_set_keymap(
+  "n",
+  "gw",
+  ":set opfunc=v:lua.web.operator<CR>g@",
+  { silent = true }
+)
+vim.api.nvim_set_keymap(
+  "x",
+  "gw",
+  "<Cmd>call v:lua.web.operator(mode())<CR>",
+  { silent = true }
+)
