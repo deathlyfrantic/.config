aaf98daeef50d674f56b07d50148e338db8ce4ae
Zandr Martin <git@zandr.me>
2021-07-22 17:45:59 -0400

    Prevent duplicate autocmds
    Also wrap autocmds in augroups more efficiently

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -37,32 +37,47 @@ local function del(id)
   end
 end
 
--- options can contain:
--- - `once` - boolean - for ++once
--- - `nested` - boolean - for ++nested
--- - `augroup` - string - to customize the augroup name
-local function add(event, pattern, callback, options)
-  local id = next_id()
-  options = options or {}
-  local augroup = options.augroup or "autocmd.lua-" .. id
-  local once
+local function autocmds_are_equal(a, b)
+  return a.event == b.event
+    and a.pattern == b.pattern
+    and string.dump(a.callback) == string.dump(b.callback)
+    and a.once == b.once
+    and a.nested == b.nested
+    and a.augroup == b.augroup
+end
+
+local function already_added(a)
+  return z.any(handlers, function(b)
+    return autocmds_are_equal(a, b)
+  end)
+end
+
+local function add(id, event, pattern, callback, options)
+  local autocmd = {
+    event = event,
+    pattern = pattern,
+    callback = callback,
+    once = options.once,
+    nested = options.nested,
+    augroup = options.augroup,
+  }
+  local once, nested
   if options.once then
     once = "++once"
-    handlers[id] = {
-      callback = function()
-        callback()
-        del(id)
-      end,
-      augroup = augroup,
-    }
-  else
-    handlers[id] = { callback = callback, augroup = augroup }
+    autocmd.callback = function()
+      callback()
+      del(id)
+    end
   end
-  local nested
+  -- `unique` should default to true, we almost never want to add a duplicate
+  -- autocmd
+  if (options.unique == nil or options.unique) and already_added(autocmd) then
+    return
+  end
+  handlers[id] = autocmd
   if options.nested then
     nested = "++nested"
   end
-  vim.cmd("augroup " .. augroup)
   vim.cmd(string.format(
     [[autocmd %s %s %s %s lua require("autocmd")._callback(%s)]],
     event,
@@ -71,21 +86,44 @@ local function add(event, pattern, callback, options)
     nested or "",
     id
   ))
-  vim.cmd("augroup END")
   return id
+end
+
+-- options can contain:
+-- - `once` - boolean - for ++once
+-- - `nested` - boolean - for ++nested
+-- - `augroup` - string - to customize the augroup name
+-- - `unique` - boolean - prevents autocmd being added again if all attributes
+--   match an existing autocmd
+local function wrap_in_augroup(name, f)
+  vim.cmd("augroup " .. name)
+  local ret = f()
+  vim.cmd("augroup END")
+  return ret
+end
+
+local function add_in_augroup(event, pattern, callback, options)
+  local id = next_id()
+  options = options or {}
+  options.augroup = options.augroup or "autocmd.lua-" .. id
+  return wrap_in_augroup(options.augroup, function()
+    return add(id, event, pattern, callback, options)
+  end)
 end
 
 local function augroup(name, f)
   local local_add = function(event, pattern, callback, options)
     options = options or {}
     options.augroup = name
-    add(event, pattern, callback, options)
+    add(next_id(), event, pattern, callback, options)
   end
-  f(local_add)
+  wrap_in_augroup(name, function()
+    return f(local_add)
+  end)
 end
 
 return {
-  add = add,
+  add = add_in_augroup,
   del = del,
   augroup = augroup,
   _callback = _callback,
