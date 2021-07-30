d122ba5a00b3213f378e238d487df87ce052dc89
Zandr Martin <git@zandr.me>
2021-07-30 09:08:47 -0400

    Translate fugitive-extras.vim to lua

---
+++ nvim/plugin/fugitive-extras.lua
@@ -0,0 +1,99 @@
+-- this is a modified version of vim-fugitive-blame-ext by Tom McDonald
+-- see: https://github.com/tommcdo/vim-fugitive-blame-ext
+local api = vim.api
+local autocmd = require("autocmd")
+local z = require("z")
+
+local subj_cmd = "git --git-dir=%s show -s --pretty=format:%%s %s"
+local full_cmd = "git --git-dir=%s show -s --format=medium --color=never %s"
+local popup_window
+
+local function log_message(commit)
+  if commit:match("^0+$") then
+    return { subj = "(Not Committed Yet)", full = "" }
+  end
+  if not vim.b.blame_messages or not vim.b.blame_messages[commit] then
+    local subj = io.popen(subj_cmd:format(vim.b.git_dir, commit)):read("*all")
+    local full = io.popen(full_cmd:format(vim.b.git_dir, commit)):read("*all")
+    -- can't insert a value into a vim.b table; have to reassign the whole thing
+    vim.b.blame_messages = vim.tbl_extend(
+      "force",
+      vim.b.blame_messages or {},
+      { [commit] = { subj = subj, full = full } }
+    )
+  end
+  return vim.b.blame_messages[commit]
+end
+
+local function truncate_message(msg)
+  local offset = 2
+  if
+    vim.o.ruler
+    or (
+      vim.o.laststatus == 0
+      or (vim.o.laststatus == 1 and #api.nvim_list_wins() == 1)
+    )
+  then
+    -- Statusline is not visible, so the ruler is. Its width is either 17
+    -- (default) or defined in 'rulerformat'.
+    local ruler_num = vim.o.rulerformat:match("^%%(%d+)%(") or 17
+    offset = offset + 1 + ruler_num
+  end
+  if vim.o.showcmd then
+    offset = offset + 11
+  end
+  local maxwidth = vim.o.columns - offset
+  if #msg > maxwidth then
+    return msg:sub(1, maxwidth - 3) .. "..."
+  end
+  return msg
+end
+
+local function get_commit_from_line()
+  return api.nvim_get_current_line():match("^[0-9A-Fa-f]+")
+end
+
+local function show_log_message()
+  local commit = get_commit_from_line()
+  local blame = log_message(commit)
+  api.nvim_echo({ { truncate_message(blame.subj) } }, false, {})
+end
+
+local function close_popup()
+  if popup_window and api.nvim_win_is_valid(popup_window) then
+    api.nvim_win_close(popup_window, true)
+  end
+  popup_window = nil
+end
+
+local function popup()
+  close_popup()
+  local commit = get_commit_from_line()
+  local blame = log_message(commit)
+  popup_window = z.popup(blame.full)
+  autocmd.add(
+    "CursorMoved,BufLeave,BufWinLeave",
+    "<buffer>",
+    close_popup,
+    { once = true, augroup = "fugitive-extras-popup" }
+  )
+end
+
+_G.fugitive_extras = { popup = popup }
+
+autocmd.augroup("fugitive-extras-blame", function(add)
+  add("BufEnter", "*.fugitiveblame", function()
+    -- needs to be separate and deferred otherwise it doesn't work ¯\_(ツ)_/¯
+    vim.defer_fn(show_log_message, 100)
+  end)
+  add("CursorMoved", "*.fugitiveblame", show_log_message)
+  add("FileType", "fugitiveblame", function()
+    api.nvim_buf_set_keymap(
+      0,
+      "n",
+      "Q",
+      "<Cmd>call v:lua.fugitive_extras.popup()<CR>",
+      { noremap = true }
+    )
+  end)
+end)
