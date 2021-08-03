3b891ace5e413624763f00327f37a9039e97f737
Zandr Martin <git@zandr.me>
2021-08-03 00:08:40 -0400

    Allow string names for scratch buffers

--- nvim/plugin/scratch.lua
+++ nvim/plugin/scratch.lua
@@ -2,45 +2,45 @@ local api = vim.api
 local autocmd = require("autocmd")
 local z = require("z")
 
-local function save_file(num)
+local function save_file(name)
   return vim.fn.expand(
-    string.format("%s/scratch-%s.txt", vim.fn.stdpath("data"), num)
+    string.format("%s/scratch-%s.txt", vim.fn.stdpath("data"), name)
   )
 end
 
-local function bufname(num)
-  return string.format("__Scratch%s__", num)
+local function bufname(name)
+  return string.format("__Scratch-%s__", name)
 end
 
 local function height()
   return math.min(10, math.floor(vim.o.lines / 3))
 end
 
-local function read(num)
+local function read(name)
   local pos = api.nvim_win_get_cursor(0)
   api.nvim_buf_set_lines(0, 0, -1, true, {})
-  local new_lines = z.collect(io.open(save_file(num)):lines())
+  local new_lines = z.collect(io.open(save_file(name)):lines())
   api.nvim_buf_set_lines(0, 0, -1, true, new_lines)
   api.nvim_win_set_cursor(0, pos)
   vim.b.ftime = os.time()
 end
 
-local function write(num)
+local function write(name)
   local contents = api.nvim_buf_get_lines(0, 0, -1, true)
-  local f = io.open(save_file(num), "w")
+  local f = io.open(save_file(name), "w")
   f:write(table.concat(contents, "\n") .. "\n")
   f:close()
   vim.b.ftime = os.time()
 end
 
-local function close_window(num)
-  local stat = vim.loop.fs_stat(save_file(num))
+local function close_window(name)
+  local stat = vim.loop.fs_stat(save_file(name))
   local bftime = vim.b.ftime
   local close = function()
-    vim.cmd(vim.fn.bufwinnr(bufname(num)) .. "close")
+    vim.cmd(vim.fn.bufwinnr(bufname(name)) .. "close")
   end
   if not stat or not bftime or bftime >= stat.mtime.sec then
-    write(num)
+    write(name)
     close()
   elseif stat and bftime and bftime < stat.mtime.sec then
     if
@@ -50,14 +50,14 @@ local function close_window(num)
         2
       ) == 1
     then
-      write(num)
+      write(name)
       close()
     end
   end
 end
 
-local function new_buffer(num)
-  vim.cmd(string.format("topleft %snew %s", height(), bufname(num)))
+local function new_buffer(name)
+  vim.cmd(string.format("topleft %snew %s", height(), bufname(name)))
   vim.opt_local.filetype = "scratch"
   vim.opt_local.buflisted = false
   vim.opt_local.bufhidden = "hide"
@@ -67,7 +67,7 @@ local function new_buffer(num)
   vim.opt_local.textwidth = 0
   vim.opt_local.winfixheight = true
   vim.opt_local.winfixwidth = true
-  vim.opt_local.statusline = "[Scratch/" .. num .. "]%=%l,%c%V%6P"
+  vim.opt_local.statusline = "[Scratch/" .. name .. "]%=%l,%c%V%6P"
   vim.opt_local.wrap = true
   vim.opt_local.linebreak = true
   api.nvim_buf_set_keymap(
@@ -81,7 +81,7 @@ local function new_buffer(num)
     0,
     "n",
     "R",
-    "lua scratch.read(" .. num .. ")",
+    "lua scratch.read(" .. name .. ")",
     { noremap = true, silent = true }
   )
   api.nvim_buf_set_keymap(
@@ -92,19 +92,19 @@ local function new_buffer(num)
     { noremap = true, silent = true }
   )
   autocmd.add("WinLeave", "<buffer>", function()
-    close_window(num)
+    close_window(name)
   end, {
-    augroup = "augroup-scratch-" .. num,
+    augroup = "augroup-scratch-" .. name,
   })
 end
 
-local function open_buffer(num)
-  local bnum = vim.fn.bufnr(bufname(num))
-  local stat = vim.loop.fs_stat(save_file(num))
+local function open_buffer(name)
+  local bnum = vim.fn.bufnr(bufname(name))
+  local stat = vim.loop.fs_stat(save_file(name))
   if bnum == -1 then
-    new_buffer(num)
+    new_buffer(name)
     if stat then
-      read(num)
+      read(name)
     end
   else
     local wnum = vim.fn.bufwinnr(bnum)
@@ -114,27 +114,24 @@ local function open_buffer(num)
       vim.cmd(wnum .. " wincmd w")
     end
     if stat and stat.mtime.sec > (vim.b.ftime or 0) then
-      read(num)
+      read(name)
     end
   end
 end
 
-local function selection(num)
+local function selection(name)
   local contents = vim.fn.getreg('"')
   local regtype = vim.fn.getregtype('"')
   vim.cmd("normal! y")
-  open_buffer(num)
+  open_buffer(name)
   api.nvim_buf_set_lines(0, 0, -1, true, {})
   api.nvim_paste(vim.fn.getreg('"'), false, -1)
   vim.fn.setreg('"', contents, regtype)
 end
 
 local function scratch(args)
-  local num
-  if args ~= nil then
-    num = tonumber(args)
-  end
-  open_buffer(num or 0)
+  local name = args or 0
+  open_buffer(name)
 end
 
 local function completion()
