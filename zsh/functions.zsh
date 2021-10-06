5d49a5c2ad1d2731192b3d965455d91e92260f0b
Zandr Martin <zandrmartin@gmail.com>
2021-10-06 09:14:32 -0400

    Use $SSH_TTY instead of $SSH_CONNECTION
    $SSH_CONNECTION persists after connections are closed, and is populated
    in other non-SSH sessions. $SSH_TTY is only populated in an SSH session.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,7 @@ function set_title {
     local prefix=""
     local mode=0 # 1 = tab, 2 = window, 0 = both
 
-    if [[ $SSH_CONNECTION != "" ]]; then
+    if [[ $SSH_TTY != "" ]]; then
         prefix="[$USER@$HOST] "
     elif [[ $USER != "zandr" ]]; then
         prefix="[$USER] "
