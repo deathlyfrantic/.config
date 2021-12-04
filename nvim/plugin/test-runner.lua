3f6d5554facba8a089ecfece188026a4e6e5a21c
Zandr Martin <zandrmartin@gmail.com>
2021-12-04 08:11:51 -0500

    Use treesitter to find nearest test name

--- nvim/plugin/test-runner.lua
+++ nvim/plugin/test-runner.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local autocmd = require("autocmd")
+local ts_utils = require("nvim-treesitter.ts_utils")
 
 local test_buffer
 
@@ -39,6 +40,43 @@ local function makefile_test()
   end
 end
 
+local function find_nearest_rust_test_from_treesitter()
+  local query = vim.treesitter.parse_query(
+    vim.bo.filetype,
+    [[((mod_item
+      (((identifier) @mod-name (#eq? @mod-name "tests"))
+        (declaration_list
+          (attribute_item (meta_item (identifier) @attr (#eq? @attr "test")))
+            . (function_item (identifier) (block)) @testfn)))
+    )]]
+  )
+  local tree = vim.treesitter.get_parser():parse()
+  local cursor_row = api.nvim_win_get_cursor(0)[1]
+  local closest = nil
+  for id, node, _ in query:iter_captures(tree[1]:root(), 0) do
+    local name = query.captures[id]
+    if name == "testfn" then
+      local row1, _, row2, _ = node:range()
+      -- rows are 0-indexed
+      row1 = row1 + 1
+      row2 = row2 + 1
+      -- if the cursor is in the test it's easy
+      if row1 <= cursor_row and row2 >= cursor_row then
+        return ts_utils.get_node_text(node:field("name")[1], 0)[1]
+      end
+      -- cursor not in this test, so check if it's near the cursor.
+      -- we're iterating in order so keep setting the closest to the current
+      -- node as long as it's before the cursor; if we've passed the cursor and
+      -- still haven't found a test, set it to the one closest to the test after
+      -- the cursor, then stop.
+      if row2 < cursor_row or closest == nil then
+        closest = node
+      end
+    end
+  end
+  return ts_utils.get_node_text(closest:field("name")[1], 0)[1]
+end
+
 local function rust(selection)
   if not vim.fn.executable("cargo") then
     return nil
@@ -54,7 +92,13 @@ local function rust(selection)
     if mod_tests_line == 0 then
       return cmd
     end
-    local nearest = find_nearest_test([[#\[test]\n\s*fn\s\+\(\w*\)(]], 2)
+    local nearest = find_nearest_rust_test_from_treesitter()
+    if nearest == nil then
+      api.nvim_err_writeln(
+        "couldn't find test from treesitter, falling back to regex"
+      )
+      nearest = find_nearest_test([[#\[test]\n\s*fn\s\+\(\w*\)(]], 2)
+    end
     return cmd:sub(1, -2) .. string.format(" %s)", nearest)
   elseif selection == "file" then
     return cmd:sub(1, -2)
@@ -63,11 +107,36 @@ local function rust(selection)
   return cmd
 end
 
+local function find_nearest_javascript_test_from_treesitter()
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if node:type() == "call_expression" then
+      local fn = node:field("function")[1]
+      local fname = ts_utils.get_node_text(fn, 0)[1]
+      if vim.tbl_contains({ "it", "describe", "test" }, fname) then
+        local args = node:field("arguments")[1]
+        local first_arg = args:child(1)
+        if first_arg:type() == "string" then
+          local text = first_arg:child(1)
+          if text:type() == "string_fragment" then
+            return ts_utils.get_node_text(text, 0)[1]
+          end
+        end
+      end
+    end
+    node = node:parent()
+  end
+end
+
 local function find_nearest_javascript_test()
-  local test = find_nearest_test(
-    [[^\s*\(it\|describe\|test\)(["'']\(.*\)["''],]],
-    3
+  local test = find_nearest_javascript_test_from_treesitter()
+  if test ~= nil then
+    return test
+  end
+  api.nvim_err_writeln(
+    "couldn't find test from treesitter, falling back to regex"
   )
+  test = find_nearest_test([[^\s*\(it\|describe\|test\)(["'']\(.*\)["''],]], 3)
   return vim.fn.substitute(test, [[\([{\[(+)\]}]\)]], [[\\\1]], "g")
 end
 
