a7ce7ef2994bb98a559e33fcdf269c153d7caa36
Zandr Martin <git@zandr.me>
2022-05-03 08:24:52 -0400

    Remove unused arg from gitsigns config function

--- nvim/plugin/packer.lua
+++ nvim/plugin/packer.lua
@@ -45,7 +45,7 @@ use({
   config = function()
     require("gitsigns").setup({
       signs = { add = { text = "+" }, change = { text = "~" } },
-      on_attach = function(bufnr)
+      on_attach = function()
         local gs = require("gitsigns")
         vim.keymap.set(
           "n",
