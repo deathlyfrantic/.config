7196e890e6129c152ba9a4e722f64c652c0ea15f
Zandr Martin <git@zandr.me>
2022-05-28 08:09:06 -0400

    Set selene.toml location for nvim lua files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,3 +1,5 @@
 if vim.startswith(vim.fn.expand("%:p", true), vim.env.VIMHOME) then
   vim.opt_local.keywordprg = ":help"
+  vim.b.ale_lua_selene_options = "--config "
+    .. vim.fn.expand("$VIMHOME/selene.toml")
 end
