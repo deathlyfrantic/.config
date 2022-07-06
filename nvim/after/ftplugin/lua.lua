c3156e47ae0047a1c3d165fc6cfb348d9f3f5d84
Zandr Martin <git@zandr.me>
2022-07-06 14:55:11 -0400

    Treat .vimrc.lua files as nvim config

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,4 +1,7 @@
-if vim.startswith(vim.fn.expand("%:p", true), vim.env.VIMHOME) then
+if
+  vim.startswith(vim.fn.expand("%:p", true), vim.env.VIMHOME)
+  or vim.fn.expand("%"):match(".vimrc.lua$")
+then
   vim.opt_local.keywordprg = ":help"
   vim.b.ale_lua_selene_options = "--config "
     .. vim.fn.expand("$VIMHOME/selene.toml")
