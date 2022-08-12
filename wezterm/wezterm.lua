1ac2042e947d8406f682b5f652bb88f15cc995b1
Zandr Martin <zandrmartin@gmail.com>
2022-08-12 07:35:44 -0400

    Maximize wezTerm window on startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -91,6 +91,12 @@ for _, location in ipairs(shell_locations) do
   end
 end
 
+-- maximize window on startup
+wezterm.on("gui-startup", function(cmd)
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 return {
   audible_bell = "Disabled",
   color_scheme = "Builtin Tango Dark",
