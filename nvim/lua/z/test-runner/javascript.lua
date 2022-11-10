9b51c4c7762614d452ca7dbf54bf9b53a1f4e9e0
Zandr Martin <zandrmartin@gmail.com>
2022-11-10 08:28:04 -0500

    Add tests for Javascript test runner

--- nvim/lua/z/test-runner/javascript.lua
+++ nvim/lua/z/test-runner/javascript.lua
@@ -1,70 +1,100 @@
 local api = vim.api
 local find_nearest_test = require("z.test-runner.utils").find_nearest_test
 
-local function find_nearest_javascript_test_from_treesitter()
-  local row, col = unpack(api.nvim_win_get_cursor(0))
-  -- treesitter rows are 0-indexed
-  local node = vim.treesitter.get_node_at_pos(0, row - 1, col)
-  while node do
-    if node:type() == "call_expression" then
-      local fn = node:field("function")[1]
-      local fname = vim.treesitter.query.get_node_text(fn, 0)
-      if vim.tbl_contains({ "it", "describe", "test" }, fname) then
-        local args = node:field("arguments")[1]
-        local first_arg = args:child(1)
-        if first_arg:type() == "string" then
-          local text = first_arg:child(1)
-          if text:type() == "string_fragment" then
-            return vim.treesitter.query.get_node_text(text, 0)
-          end
-        end
+local function find_nearest_treesitter()
+  local query = vim.treesitter.parse_query(
+    vim.bo.filetype,
+    [[((call_expression
+      function: ((identifier) @fn (#match? @fn "^(describe|it|test|context)$")) 
+      arguments: (arguments . (string ((string_fragment) @testname)))) @testfn)
+    ]]
+  )
+  local tree = vim.treesitter.get_parser():parse()
+  local cursor_row = api.nvim_win_get_cursor(0)[1]
+  local closest = nil
+  for id, node, _ in query:iter_captures(tree[1]:root(), 0) do
+    local name = query.captures[id]
+    if name == "testname" then
+      local row1, _, row2, _ = node:range()
+      -- rows are 0-indexed
+      row1 = row1 + 1
+      row2 = row2 + 1
+      -- if the cursor is in the test it's easy
+      if row1 <= cursor_row and row2 >= cursor_row then
+        return vim.treesitter.query.get_node_text(node, 0)
+      end
+      -- cursor not in this test, so check if it's near the cursor.
+      -- we're iterating in order so keep setting the closest to the current
+      -- node as long as it's before the cursor; if we've passed the cursor and
+      -- still haven't found a test, set it to the one closest to the test after
+      -- the cursor, then stop.
+      if row2 < cursor_row or closest == nil then
+        closest = node
       end
     end
-    node = node:parent()
   end
+  if closest ~= nil then
+    return vim.treesitter.query.get_node_text(closest, 0)
+  end
+  return nil
 end
 
-local function find_nearest_javascript_test()
-  local test = find_nearest_javascript_test_from_treesitter()
-  if test ~= nil then
-    return test
-  end
-  api.nvim_err_writeln(
-    "couldn't find test from treesitter, falling back to regex"
+local function find_nearest_regex()
+  vim.notify(
+    "couldn't find test from treesitter, falling back to regex",
+    vim.log.levels.ERROR
   )
-  test = find_nearest_test([[^\s*\(it\|describe\|test\)(["'']\(.*\)["''],]], 3)
-  return vim.fn.substitute(test, [[\([{\[(+)\]}]\)]], [[\\\1]], "g")
+  local test = find_nearest_test(
+    [[^\s*\(it\|describe\|test\|context\)(["'']\(.*\)["''],]],
+    3
+  )
+  if test ~= nil then
+    return vim.fn.substitute(test, [[\([{\[(+)\]}]\)]], [[\\\1]], "g")
+  end
+  return nil
+end
+
+local function find_nearest()
+  return find_nearest_treesitter() or find_nearest_regex()
 end
 
 local function npm_or_yarn()
-  if vim.fn.findfile("yarn.lock", ";") ~= "" then
-    return "yarn"
+  if vim.b.z_test_runner_npm_or_yarn == nil then
+    if vim.fn.findfile("yarn.lock", ";") ~= "" then
+      vim.b.z_test_runner_npm_or_yarn = "yarn"
+    else
+      vim.b.z_test_runner_npm_or_yarn = "npm"
+    end
   end
-  return "npm"
+  return vim.b.z_test_runner_npm_or_yarn
 end
 
-local function javascript_mocha(selection, pretest)
+local function mocha(selection, pretest)
   local cmd = "npx mocha -- spec " .. vim.fs.normalize(api.nvim_buf_get_name(0))
-  if #pretest > 0 then
+  if pretest and #pretest > 0 then
     cmd = pretest .. " && " .. cmd
   end
   if selection == "nearest" then
-    return cmd
-      .. " --grep="
-      .. vim.fn.shellescape(find_nearest_javascript_test())
+    local nearest = find_nearest()
+    if nearest ~= nil then
+      return cmd .. " --grep=" .. vim.fn.shellescape(find_nearest())
+    end
   elseif selection == "file" then
     return cmd
   end
   return npm_or_yarn() .. " test"
 end
 
-local function javascript_jest(selection)
+local function jest(selection)
   local cmd = npm_or_yarn() .. " test"
   if vim.startswith(cmd, "npm") then
     cmd = cmd .. " --"
   end
   if selection == "nearest" then
-    return cmd .. " -t " .. vim.fn.shellescape(find_nearest_javascript_test())
+    local nearest = find_nearest()
+    if nearest ~= nil then
+      return cmd .. " -t " .. vim.fn.shellescape(find_nearest())
+    end
   elseif selection == "file" then
     return cmd .. " " .. vim.fs.normalize(api.nvim_buf_get_name(0))
   end
@@ -80,13 +110,20 @@ local function test(selection)
   local scripts = package.scripts or {}
   local test_cmd = scripts.test or ""
   if test_cmd:match("mocha") then
-    return javascript_mocha(selection, scripts.pretest or "")
+    return mocha(selection, scripts.pretest or "")
   elseif test_cmd:match("jest") then
-    return javascript_jest(selection)
+    return jest(selection)
   end
   if #test_cmd > 0 then
     return npm_or_yarn() .. " test"
   end
 end
 
-return { test = test }
+return {
+  find_nearest_treesitter = find_nearest_treesitter,
+  find_nearest_regex = find_nearest_regex,
+  npm_or_yarn = npm_or_yarn,
+  mocha = mocha,
+  jest = jest,
+  test = test,
+}
---
+++ nvim/tests/test-runner/javascript_spec.lua
@@ -0,0 +1,295 @@
+local stub = require("luassert.stub")
+local match = require("luassert.match")
+local dedent = require("plenary.strings").dedent
+local javascript = require("z.test-runner.javascript")
+
+local function set_cursor(row, col)
+  vim.api.nvim_win_set_cursor(0, { row, col or 0 })
+end
+
+describe("test-runner/javascript", function()
+  local template = dedent([[
+    describe("tests", () => {
+      it("test1", () => {
+        // ...
+      });
+
+      test('test2', async () => {
+        // ...
+      });
+
+      context("test3", function() {
+        // ...
+      });
+
+      it(`test4`, () => {
+        // ...
+      });
+
+      it(test5(), {
+        // ...
+      });
+    });
+
+    ]])
+
+  before_each(function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, template:split("\n"))
+    vim.bo.filetype = "javascript"
+  end)
+
+  after_each(function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+    -- doing it this way prevents the "unknown filetype" error from printing
+    vim.cmd("silent! setlocal filetype ''")
+  end)
+
+  describe("treesitter", function()
+    it("finds the correct test based on cursor position", function()
+      local test_cases = {
+        { 1, "tests" },
+        { 3, "test1" },
+        { 5, "test1" },
+        { 7, "test2" },
+        { 11, "test3" },
+        { 15, "test3" },
+        { 19, "test3" },
+      }
+      for _, test_case in ipairs(test_cases) do
+        local line, expected = unpack(test_case)
+        set_cursor(line)
+        assert.equals(expected, javascript.find_nearest_treesitter())
+      end
+    end)
+
+    it("returns nil if cursor is not in test/describe block", function()
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+      assert.is_nil(javascript.find_nearest_treesitter())
+    end)
+  end)
+
+  describe("regex", function()
+    local notify
+
+    before_each(function()
+      notify = stub(vim, "notify")
+    end)
+
+    after_each(function()
+      notify:revert()
+    end)
+
+    it("logs error if called (because treesitter didn't find test)", function()
+      set_cursor(3)
+      assert.equals("test1", javascript.find_nearest_regex())
+      assert.stub(notify).called_with(match.string(), vim.log.levels.ERROR)
+    end)
+
+    it("finds the correct test based on cursor position", function()
+      local test_cases = {
+        { 1, "tests" },
+        { 2, "test1" },
+        { 4, "test1" },
+        { 7, "test2" },
+        { 9, "test2" }, -- finds previous test instead
+        { 11, "test3" },
+        { 15, "test3" }, -- finds previous test instead
+        { 19, "test3" }, -- finds previous test instead
+        { 22, "test3" }, -- finds previous test instead
+      }
+      for _, test_case in ipairs(test_cases) do
+        local line, expected = unpack(test_case)
+        set_cursor(line)
+        assert.equals(expected, javascript.find_nearest_regex())
+      end
+    end)
+
+    it("returns nil if it can't find a test", function()
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+      assert.is_nil(javascript.find_nearest_regex())
+    end)
+  end)
+
+  describe("npm or yarn", function()
+    local findfile
+
+    before_each(function()
+      findfile = stub(vim.fn, "findfile")
+    end)
+
+    after_each(function()
+      findfile:revert()
+      vim.b.z_test_runner_npm_or_yarn = nil
+    end)
+
+    it("returns yarn if a yarn.lock file is found", function()
+      findfile.returns("yarn.lock")
+      assert.equals("yarn", javascript.npm_or_yarn())
+    end)
+
+    it("returns npm if a yarn.lock file is not found", function()
+      findfile.returns("")
+      assert.equals("npm", javascript.npm_or_yarn())
+    end)
+
+    it("caches value per buffer", function()
+      assert.is_nil(vim.b.z_test_runner_npm_or_yarn)
+      findfile.returns("")
+      assert.equals("npm", javascript.npm_or_yarn())
+      assert.equals(vim.b.z_test_runner_npm_or_yarn, "npm")
+    end)
+  end)
+
+  describe("mocha", function()
+    local nvim_buf_get_name
+
+    before_each(function()
+      nvim_buf_get_name =
+        stub(vim.api, "nvim_buf_get_name").returns("/foobar/file.js")
+    end)
+
+    after_each(function()
+      nvim_buf_get_name:revert()
+    end)
+
+    it("returns command for nearest", function()
+      set_cursor(3)
+      assert.equals(
+        "npx mocha -- spec /foobar/file.js --grep='test1'",
+        javascript.mocha("nearest")
+      )
+    end)
+
+    it("adds pretest if provided", function()
+      set_cursor(3)
+      assert.equals(
+        "pretest && npx mocha -- spec /foobar/file.js --grep='test1'",
+        javascript.mocha("nearest", "pretest")
+      )
+    end)
+
+    it("returns file command for 'file' selection", function()
+      assert.equals(
+        "npx mocha -- spec /foobar/file.js",
+        javascript.mocha("file")
+      )
+    end)
+
+    it("returns generic command for 'all' selection", function()
+      assert.equals("npm test", javascript.mocha("all"))
+    end)
+
+    it("returns generic test command if nearest test is nil", function()
+      local notify = stub(vim, "notify")
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+      assert.equals("npm test", javascript.mocha("nearest"))
+      notify:revert()
+    end)
+  end)
+
+  describe("jest", function()
+    local nvim_buf_get_name
+
+    before_each(function()
+      nvim_buf_get_name =
+        stub(vim.api, "nvim_buf_get_name").returns("/foobar/file.js")
+    end)
+
+    after_each(function()
+      nvim_buf_get_name:revert()
+    end)
+
+    it("returns command for nearest", function()
+      set_cursor(3)
+      assert.equals("npm test -- -t 'test1'", javascript.jest("nearest"))
+    end)
+
+    it("returns file command for 'file' selection", function()
+      assert.equals("npm test -- /foobar/file.js", javascript.jest("file"))
+    end)
+
+    it("returns generic command for 'all' selection", function()
+      assert.equals("npm test --", javascript.jest("all"))
+    end)
+
+    it("returns generic test command if nearest test is nil", function()
+      local notify = stub(vim, "notify")
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+      assert.equals("npm test --", javascript.jest("nearest"))
+      notify:revert()
+    end)
+  end)
+
+  describe("test command", function()
+    local findfile, ioopen, nvim_buf_get_name
+
+    local packagejson = [[
+      {
+        "scripts": {
+          "pretest": "pretest.sh",
+          "test": "%s"
+        }
+      }
+    ]]
+
+    before_each(function()
+      findfile = stub(vim.fn, "findfile")
+      ioopen = stub(io, "open")
+      nvim_buf_get_name =
+        stub(vim.api, "nvim_buf_get_name").returns("/foobar/file.js")
+    end)
+
+    after_each(function()
+      findfile:revert()
+      ioopen:revert()
+      nvim_buf_get_name:revert()
+    end)
+
+    it("returns nil if package.json is not found", function()
+      findfile.returns("")
+      assert.is_nil(javascript.test())
+    end)
+
+    it(
+      "returns mocha command if package.json scripts.test contains 'mocha'",
+      function()
+        set_cursor(3)
+        findfile.returns("/foobar/package.json")
+        ioopen.returns({
+          read = function()
+            return packagejson:format("foo mocha bar")
+          end,
+        })
+        assert.equals(
+          "pretest.sh && npx mocha -- spec /foobar/file.js --grep='test1'",
+          javascript.test("nearest")
+        )
+      end
+    )
+
+    it(
+      "returns jest command if package.json scripts.test contains 'jest'",
+      function()
+        set_cursor(3)
+        findfile.returns("/foobar/package.json")
+        ioopen.returns({
+          read = function()
+            return packagejson:format("foo jest bar")
+          end,
+        })
+        assert.equals("npm test -- -t 'test1'", javascript.test("nearest"))
+      end
+    )
+
+    it("returns generic test command for other cases", function()
+      set_cursor(3)
+      findfile.returns("/foobar/package.json")
+      ioopen.returns({
+        read = function()
+          return packagejson:format("foo bar")
+        end,
+      })
+      assert.equals("npm test", javascript.test("nearest"))
+    end)
+  end)
+end)
