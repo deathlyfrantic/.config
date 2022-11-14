3d0a7c86ed5d6e08a4ae85c1649b70b1c771155b
Zandr Martin <zandrmartin@gmail.com>
2022-11-14 17:02:54 -0500

    Conditional Python docstring regex highlighting

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,5 +1,8 @@
 -- highlight docstrings as comments
 local function docstring_highlight()
+  if vim.bo.syntax == "" then
+    return
+  end
   vim.cmd.syntax(
     [[region pythonDocstring start=+^\s*[uU]\?[rR]\?\%("""\|'''\)+ ]]
       .. [[end=+\%("""\|'''\)+ keepend excludenl ]]
