562c9c1ebccf2f1dccad67140de9dc45b32c8403
Zandr Martin <zandrmartin@gmail.com>
2022-11-22 20:36:29 -0500

    Undo test runner class situation
    More-or-less backs out 35354b29.

--- nvim/lua/z/test-runner/init.lua
+++ nvim/lua/z/test-runner/init.lua
@@ -1,18 +1,14 @@
 local TermWindow = require("z.term-window")
 
-local mt = {
-  term_window = nil,
-  runners = {
-    javascript = require("z.test-runner.javascript").test,
-    make = require("z.test-runner.makefile").test,
-    python = require("z.test-runner.python").test,
-    rust = require("z.test-runner.rust").test,
-    typescript = require("z.test-runner.javascript").test,
-  },
+local term_window = nil
+
+local runners = {
+  javascript = require("z.test-runner.javascript").test,
+  make = require("z.test-runner.makefile").test,
+  python = require("z.test-runner.python").test,
+  rust = require("z.test-runner.rust").test,
+  typescript = require("z.test-runner.javascript").test,
 }
-mt.__index = mt
-local TestRunner = {}
-setmetatable(TestRunner, mt)
 
 function on_exit(_, exit_code)
   if exit_code == 0 then
@@ -24,32 +20,32 @@ function on_exit(_, exit_code)
   end
 end
 
-function TestRunner.run(self, cmd, close)
-  if not self.term_window then
-    self.term_window = TermWindow.new({ close_on_success = close })
-    self.term_window:on("Exit", function(...)
+function run(cmd, close)
+  if not term_window then
+    term_window = TermWindow.new({ close_on_success = close })
+    term_window:on("Exit", function(...)
       on_exit(...)
     end)
-    self.term_window:on("BufDelete", function()
-      self.term_window = nil
+    term_window:on("BufDelete", function()
+      term_window = nil
     end)
-    self.term_window:on("BufAdd", function()
-      local buf = self.term_window.buffer
+    term_window:on("BufAdd", function()
+      local buf = term_window.buffer
       vim.keymap.set("n", "R", function()
         -- rerun the tests
         vim.bo[buf].modified = false
         vim.bo[buf].modifiable = true
-        self:run(cmd, self.term_window.close_on_success)
+        run(cmd, term_window.close_on_success)
       end, { buffer = buf, silent = true })
     end)
   else
     -- do this here in case we're reusing the TermWindow for another test
-    self.term_window.close_on_success = close
+    term_window.close_on_success = close
   end
-  self.term_window:run(cmd)
+  term_window:run(cmd)
 end
 
-function TestRunner.test(self, selection, close)
+function test(selection, close)
   local test_cmds, errs = {}, {}
   local filetype = vim.bo.filetype
   if type(vim.b.test_command) == "string" then
@@ -70,34 +66,29 @@ function TestRunner.test(self, selection, close)
         vim.api.nvim_exec("echo b:test_command." .. selection .. "()", true)
       )
     end
-  elseif self.runners[filetype] ~= nil then
-    local cmd = self.runners[filetype](selection)
+  elseif runners[filetype] ~= nil then
+    local cmd = runners[filetype](selection)
     if cmd ~= nil then
       table.insert(test_cmds, cmd)
     end
   else
-    table.insert(
-      errs,
-      string.format("No tests available for filetype '%s'", filetype)
-    )
+    table.insert(errs, "No tests available for filetype '" .. filetype .. "'")
   end
-  local maketest = self.runners.make()
+  local maketest = runners.make()
   if maketest ~= nil then
     table.insert(test_cmds, maketest)
   else
     table.insert(errs, "no `Makefile` found")
   end
   if #test_cmds > 0 then
-    self:run(test_cmds[1], close)
+    run(test_cmds[1], close)
   else
     vim.notify(table.concat(errs, " and "), vim.log.levels.ERROR)
   end
 end
 
 return {
-  test = function(...)
-    TestRunner:test(...)
-  end,
+  test = test,
   -- expose this for use in local config files etc
   find_nearest_test = require("z.test-runner.utils").find_nearest_test,
 }
