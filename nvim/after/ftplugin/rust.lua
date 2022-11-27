fbd811af72a8e7c916415c8c575d225cf35e5ad5
Zandr Martin <zandrmartin@gmail.com>
2022-11-27 12:27:18 -0500

    Pass edition flag to rustfmt

---
+++ nvim/after/ftplugin/rust.lua
@@ -0,0 +1,21 @@
+local function populate_rustfmt_edition()
+  local cargo_toml_path = vim.fn.findfile("Cargo.toml", ".;")
+  if cargo_toml_path == "" then
+    return
+  end
+  local cargo_toml = io.open(cargo_toml_path)
+  for line in cargo_toml:lines() do
+    if vim.startswith(line:trim(), "edition") then
+      local edition = line:split("=")[2]:match([["(.+)"]])
+      if edition then
+        vim.b.ale_rust_rustfmt_options = "--edition " .. edition
+        break
+      end
+    end
+  end
+  io.close(cargo_toml)
+end
+
+if not vim.b.ale_rust_rustfmt_options then
+  populate_rustfmt_edition()
+end
