de976354191f0dac879f2416131840ae1eb7afb2
Zandr Martin <zandrmartin@gmail.com>
2022-12-14 06:42:47 -0500

    Use Pydoc as keywordprg in Python files

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,3 +1,5 @@
+local z = require("z")
+
 -- highlight docstrings as comments
 local function docstring_highlight()
   if vim.bo.syntax == "" then
@@ -16,3 +18,17 @@ vim.api.nvim_create_autocmd("Syntax", {
   callback = docstring_highlight,
   group = vim.api.nvim_create_augroup("python-ftplugin", {}),
 })
+
+local function pydoc_help(args)
+  local output = z.collect(io.popen("python3 -m pydoc " .. args.args):lines())
+  if
+    #output == 0 or vim.startswith(output[1], "No Python documentation found")
+  then
+    vim.notify("E149: Sorry, no help for " .. args.args, vim.log.levels.ERROR)
+    return
+  end
+  z.help(output)
+end
+
+vim.api.nvim_buf_create_user_command(0, "PydocHelp", pydoc_help, { nargs = 1 })
+vim.bo.keywordprg = ":PydocHelp"
