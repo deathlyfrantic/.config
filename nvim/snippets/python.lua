ed0fc7c2a8a2527127c55943ff0389b943a49cb8
Zandr Martin <zandrmartin@gmail.com>
2023-01-03 11:05:28 -0500

    Add Python __init__ method snippet

--- nvim/snippets/python.lua
+++ nvim/snippets/python.lua
@@ -1,3 +1,7 @@
+local ls = require("luasnip")
+local i = ls.insert_node
+local f = ls.function_node
+local fmt = require("luasnip.extras.fmt").fmt
 local make = require("z.snippets").make
 
 return make({
@@ -16,4 +20,38 @@ return make({
 
 
     ]],
+  init = fmt(
+    [[
+      def __init__(self, {}):
+          {}
+          {}
+    ]],
+    {
+      i(1),
+      f(function(args)
+        local pieces =
+          vim.split(args[1][1] or "", ",", { plain = true, trimempty = true })
+        if #pieces == 0 then
+          return ""
+        end
+        local words = {}
+        for _, piece in ipairs(pieces) do
+          local word = piece:trim():match("^[%a_][%w_]*")
+          if word then
+            table.insert(words, word)
+          end
+        end
+        local ret = {}
+        for n, word in ipairs(words) do
+          local indent = ""
+          if n > 1 then
+            indent = "    "
+          end
+          table.insert(ret, indent .. "self." .. word .. " = " .. word)
+        end
+        return ret
+      end, { 1 }),
+      i(0),
+    }
+  ),
 })
