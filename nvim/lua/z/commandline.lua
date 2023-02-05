ebeccda3a5cca4bee8117a9d1ffde425515264d6
Zandr Martin <zandrmartin@gmail.com>
2023-02-05 14:53:27 -0500

    Reorganize commandline code, add tests

---
+++ nvim/lua/z/commandline.lua
@@ -0,0 +1,67 @@
+local function poscmd()
+  return vim.fn.getcmdpos(), vim.fn.getcmdline()
+end
+
+local function kill_line()
+  local pos, cmd = poscmd()
+  if pos == 1 then
+    return ""
+  end
+  return cmd:sub(1, pos - 1)
+end
+
+local function delete_word()
+  local pos, cmd = poscmd()
+  local stop = pos
+  while cmd:sub(stop + 1, stop + 1):match("[^%w]") and stop < #cmd do
+    stop = stop + 1
+  end
+  while stop < #cmd do
+    if cmd:sub(stop + 1, stop + 1):match("[^%w]") then
+      break
+    end
+    stop = stop + 1
+  end
+  if pos == 1 then
+    return cmd:sub(stop + 1)
+  end
+  return cmd:sub(0, pos - 1) .. cmd:sub(stop + 1)
+end
+
+local function bwd_by_word()
+  local pos, cmd = poscmd()
+  if pos ~= 1 then
+    local saw_letter = false
+    for i = pos - 1, 0, -1 do
+      saw_letter = saw_letter or cmd:sub(i, i):match("%w") ~= nil
+      if cmd:sub(i, i):match("%w") == nil and saw_letter then
+        break
+      end
+      vim.fn.setcmdpos(i)
+    end
+  end
+  return cmd
+end
+
+local function fwd_by_word()
+  local pos, cmd = poscmd()
+  local cmdlen = #cmd
+  if pos < cmdlen then
+    local saw_letter = false
+    for i = pos, cmdlen do
+      saw_letter = saw_letter or cmd:sub(i, i):match("%w") ~= nil
+      if cmd:sub(i, i):match("%w") == nil and saw_letter then
+        break
+      end
+      vim.fn.setcmdpos(i + 1)
+    end
+  end
+  return cmd
+end
+
+return {
+  kill_line = kill_line,
+  bwd_by_word = bwd_by_word,
+  fwd_by_word = fwd_by_word,
+  delete_word = delete_word,
+}
--- nvim/plugin/commandline.lua
+++ nvim/plugin/commandline.lua
@@ -1,70 +1,5 @@
-local function poscmd()
-  return vim.fn.getcmdpos(), vim.fn.getcmdline()
-end
-
-local function kill_line()
-  local pos, cmd = poscmd()
-  if pos == 1 then
-    return ""
-  end
-  return cmd:sub(1, pos - 1)
-end
-
-local function delete_word()
-  local pos, cmd = poscmd()
-  local stop = pos
-  while cmd:sub(stop + 1, stop + 1):match("[^%w]") and stop < #cmd do
-    stop = stop + 1
-  end
-  while stop < #cmd do
-    if cmd:sub(stop + 1, stop + 1):match("[^%w]") then
-      break
-    end
-    stop = stop + 1
-  end
-  if pos == 1 then
-    return cmd:sub(stop + 1)
-  end
-  return cmd:sub(0, pos - 1) .. cmd:sub(stop + 1)
-end
-
-local function bwd_by_word()
-  local pos, cmd = poscmd()
-  if pos ~= 1 then
-    local saw_letter = false
-    local i = pos - 1
-    while i > 0 do
-      i = i - 1
-      saw_letter = saw_letter or cmd:sub(i + 1, i + 1):match("%w") ~= nil
-      if cmd:sub(i + 1, i + 1):match("%w") == nil and saw_letter then
-        break
-      end
-      vim.fn.setcmdpos(i + 1)
-    end
-  end
-  return cmd
-end
-
-local function fwd_by_word()
-  local pos, cmd = poscmd()
-  local cmdlen = #cmd
-  if pos < cmdlen then
-    local saw_letter = false
-    local i = pos - 1
-    while i < cmdlen do
-      i = i + 1
-      saw_letter = saw_letter or cmd:sub(i + 1, i + 1):match("%w") ~= nil
-      if cmd:sub(i + 1, i + 1):match("%w") == nil and saw_letter then
-        break
-      end
-      vim.fn.setcmdpos(i + 2)
-    end
-  end
-  return cmd
-end
-
 local function call(fn)
-  return ([[<C-\>e luaeval("commandline.%s()")<CR>]]):format(fn)
+  return ([[<C-\>e luaeval("require('z.commandline').%s()")<CR>]]):format(fn)
 end
 
 vim.keymap.set("c", "<C-a>", "<Home>")
@@ -79,10 +14,3 @@ vim.keymap.set("c", "<C-k>", call("kill_line"))
 vim.keymap.set("c", "<M-d>", call("delete_word"))
 vim.keymap.set("c", "<M-b>", call("bwd_by_word"))
 vim.keymap.set("c", "<M-f>", call("fwd_by_word"))
-
-_G.commandline = {
-  kill_line = kill_line,
-  bwd_by_word = bwd_by_word,
-  fwd_by_word = fwd_by_word,
-  delete_word = delete_word,
-}
