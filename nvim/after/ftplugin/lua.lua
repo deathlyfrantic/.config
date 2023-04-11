776df51562e4cc823db90e8bf5370b5951174fb0
Zandr Martin <zandrmartin@gmail.com>
2023-04-11 07:51:26 -0400

    Use `lua_omnifunc` in Neovim Lua config files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -4,6 +4,7 @@ if
   or vim.endswith(bufname, ".vimrc.lua")
 then
   vim.opt_local.keywordprg = ":help"
+  vim.opt_local.omnifunc = "v:lua.vim.lua_omnifunc"
   vim.b.ale_lua_selene_options = "--config "
     .. vim.fs.normalize("$VIMHOME/selene.toml")
 end
