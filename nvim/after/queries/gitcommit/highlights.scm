c88fdc54787ef70197cdc264c13845a3e845db7b
Zandr Martin <zandrmartin@gmail.com>
2024-07-11 09:11:02 -0400

    Fix gitcommit highlighting for new parser
    The "overflow" node type was removed in this PR:
    https://github.com/gbprod/tree-sitter-gitcommit/pull/68

--- nvim/after/queries/gitcommit/highlights.scm
+++ nvim/after/queries/gitcommit/highlights.scm
@@ -1,4 +1,11 @@
 ; extends
 
-(subject (overflow) @gitcommit_error)
 (ERROR) @gitcommit_error
+
+((subject) @gitcommit_error
+  (#vim-match? @gitcommit_error ".\{50,}")
+  (#offset! @gitcommit_error 0 50 0 0))
+
+((message_line) @gitcommit_error
+  (#vim-match? @gitcommit_error ".\{72,}")
+  (#offset! @gitcommit_error 0 72 0 0))
