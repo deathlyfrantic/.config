97c20133b34da06945047d7e9ef901160004ceed
Zandr Martin <zandrmartin@gmail.com>
2024-09-02 08:40:56 -0400

    Use built-in comment text object

--- nvim/lua/text-objects/init.lua
+++ nvim/lua/text-objects/init.lua
@@ -2,6 +2,13 @@ local M = {}
 
 function M.init()
   require("text-objects.indent").init()
+  -- comment text object is built in, just remap it
+  vim.keymap.set(
+    { "o", "v" },
+    "ic",
+    [[:<C-u>lua require("vim._comment").textobject()<CR>]],
+    { silent = true }
+  )
 end
 
 return M
--- nvim/plugin/packages.lua
+++ nvim/plugin/packages.lua
@@ -89,7 +89,6 @@ pm.add({
 pm.add(
   "Julian/vim-textobj-variable-segment",
   "kana/vim-textobj-user",
-  "glts/vim-textobj-comment",
   "deathlyfrantic/vim-textobj-blanklines"
 )
 
