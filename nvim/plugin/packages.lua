9e3e551e08cefb37940fa4c788af87de5549ac27
Zandr Martin <zandrmartin@gmail.com>
2024-10-28 14:41:06 -0400

    No more Bitbucket 😌

--- nvim/plugin/packages.lua
+++ nvim/plugin/packages.lua
@@ -331,4 +331,4 @@ pm.add({
     vim.keymap.set("", "<leader>gb", ":GBrowse!<CR>", { silent = true })
     vim.keymap.set("n", "<leader>gw", "<Cmd>Gwrite!<CR>", { silent = true })
   end,
-}, "tommcdo/vim-fubitive", "tpope/vim-rhubarb")
+}, "tpope/vim-rhubarb")
