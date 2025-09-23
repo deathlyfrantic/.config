20f2d06fa4979d5729dcefd949bb919d5cc38137
Zandr Martin <zandrmartin@gmail.com>
2025-09-23 10:23:33 -0400

    Add keymap to insert `:=` for Go

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,3 +1,9 @@
+local utils = require("utils")
+
 vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 8
 vim.opt_local.textwidth = 80
+
+vim.keymap.set("i", ";;", function()
+  return utils.insert_token(":=")
+end, { buffer = true, expr = true })
